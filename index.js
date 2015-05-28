0bac5794916f3e8a4b454f0b8cc395f35e61a395
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-05-27 23:31:11 -0400

    refactoring

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ var git = require('gift');
 var quote = require('quote');
 var join = require('path').join;
 
-function directToSubApp(req, res, next) {
-  var from = req.headers.referer;
-  // something like http://localhost:8765/charts-lib
-  console.log('req url %s path %s from %s', req.url, req.path, from);
-  var webApp = from.split('/')[3];
-  if (webApp) {
-    console.log('web app name', quote(webApp));
-    req.url = '/' + join(webApp, req.url);
-    console.log('rewritten request url %s', req.url);
-  }
-  next();
-}
+var directToSubApp = require('./src/sub-app');
 
 var app = express();
 app.use(morgan('dev'));
