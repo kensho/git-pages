3688d34ec5cc04451ac01824a7b8808fa1a55842
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-06-15 15:18:07 -0400

    refactoring subapp middleware to handle null referer better

--- src/sub-app.js
+++ src/sub-app.js
@@ -4,12 +4,15 @@ var getAppName = require('./app-name');
 
 function directToSubApp(req, res, next) {
   var from = req.headers.referer;
-  console.log('req url %s path %s from %s', req.url, req.path, from);
-  var webApp = getAppName(from);
-  if (webApp) {
-    console.log('web app name', quote(webApp));
-    req.url = '/' + join(webApp, req.url);
-    console.log('rewritten request url %s', req.url);
+  if (from) {
+    console.log('req url %s path %s from %s', req.url, req.path, from);
+    var webApp = getAppName(from);
+    if (webApp) {
+      console.log('web app name', quote(webApp));
+      req.url = '/' + join(webApp, req.url);
+      req.originalUrl = req.path = req.url;
+      console.log('rewritten request url %s', req.url);
+    }
   }
   next();
 }
